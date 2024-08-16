2570de877a54a5b5a497e10cefd3bf67927661cd
Pedro <priccis@users.noreply.github.com>
2024-08-16 17:20:26 -0400

    feat: support injecting new script

--- src/content-script.ts
+++ src/content-script.ts
@@ -2,6 +2,7 @@ import type { Rule } from "./types";
 
 (async () => {
   const currentHostname = document.location.hostname;
+
   function replaceRules(rules: Rule[]) {
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
@@ -9,12 +10,14 @@ import type { Rule } from "./types";
           if ("tagName" in node && node.tagName === "SCRIPT" && "src" in node) {
             const foundRule = rules.find(
               (rule) =>
+                rule.type === "replace" &&
                 rule.hostname === currentHostname &&
                 rule.enabled &&
+                rule.selector &&
                 (node.src as string).match(rule.selector)
             );
             if (foundRule) {
-              node.src = foundRule.url;
+              node.src = foundRule.url!;
             }
           }
         });
@@ -27,6 +30,21 @@ import type { Rule } from "./types";
     });
   }
 
+  function injectRules(rules: Rule[]) {
+    rules.forEach((rule) => {
+      if (
+        rule.type === "inject" &&
+        rule.hostname === currentHostname &&
+        rule.enabled &&
+        rule.url
+      ) {
+        const script = document.createElement("script");
+        script.src = rule.url;
+        document.head.appendChild(script);
+      }
+    });
+  }
+
   chrome.runtime.onMessage.addListener((request) => {
     if (!("rules" in request)) return;
     const rules = request.rules as Rule[];
@@ -36,6 +54,7 @@ import type { Rule } from "./types";
     console.log("BundleReplacer loaded");
 
     replaceRules(rules);
+    injectRules(rules);
   });
 
   chrome.runtime.sendMessage({ type: "getRules" });
